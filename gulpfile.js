601cd3961cf46bdbf8a8eb79c954ce37426a35ec
luckyrat <luckyrat@musites.com>
2017-04-29 00:06:09 +0100

    Packaging build step

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require("gulp");
 var tslint = require("gulp-tslint");
 var ts = require("gulp-typescript");
 var sourcemaps = require('gulp-sourcemaps');
+var zip = require('gulp-zip');
 
 gulp.task("default", [ "build" ]);
 gulp.task("build", ["ts:background", "ts:popup", "ts:page" ]);
@@ -73,3 +74,46 @@ gulp.task('watch', ['build'], function() {
     gulp.watch(['common/**/*.ts', 'page/**/*.ts'], ['ts:page']);
     gulp.watch(['common/**/*.ts', 'background/**/*.ts'], ['ts:background']);
 });
+
+gulp.task('collect', function() {
+    gulp.src('page/page.js')
+		.pipe(gulp.dest('build/page'));
+    gulp.src('page/page.js')
+		.pipe(gulp.dest('build/page'));
+    gulp.src('popup/popup.js')
+		.pipe(gulp.dest('build/popup'));
+    gulp.src('popup/popup.css')
+		.pipe(gulp.dest('build/popup'));
+    gulp.src('popup/popup.html')
+		.pipe(gulp.dest('build/popup'));
+    gulp.src('settings/settings.html')
+		.pipe(gulp.dest('build/settings'));
+	gulp.src('common/*.js')
+		.pipe(gulp.dest('build/common'));
+	gulp.src('common/images/**')
+		.pipe(gulp.dest('build/common/images'));
+	gulp.src('background/*.js')
+		.pipe(gulp.dest('build/background'));
+	gulp.src('_locales/**')
+		.pipe(gulp.dest('build/_locales'));
+	return gulp.src('manifest.json')
+        .pipe(gulp.dest('build'));
+});
+
+gulp.task('zip', ['collect'], function() {
+	var manifest = require('./manifest'),
+		distFileName = manifest.name + '-v' + manifest.version + '.zip';
+	return gulp.src('build/**')
+		.pipe(zip(distFileName))
+		.pipe(gulp.dest('dist'));
+});
+
+gulp.task('xpi', ['collect'], function() {
+	var manifest = require('./manifest'),
+		distFileName = manifest.name + '-v' + manifest.version + '.xpi';
+	return gulp.src('build/**')
+		.pipe(zip(distFileName))
+		.pipe(gulp.dest('dist'));
+});
+
+gulp.task('package', ['xpi','zip']);
