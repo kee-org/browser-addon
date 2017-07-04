7142fa3f7aac4f4938866b819451dcc8c439096c
luckyrat <luckyrat@musites.com>
2017-07-04 20:20:34 +0100

    Separate concept of finding submit button and clicking submit

--- page/formFilling.ts
+++ page/formFilling.ts
@@ -920,9 +920,11 @@ class FormFilling {
         }
     }
 
-    // Submit a form
-    private submitForm (form)
+    private findSubmitButton (form: HTMLFormElement)
     {
+
+        //TODO:c: Is form.ownerDocument valid on a pseudo form?! If not, make it so or change algorithm below
+
         // Priority 1: button within form provided: @type != reset
         // Priority 1: button outside form with @form attribute provided: @type != reset
         // Priority 2: input @type=submit within form
@@ -1024,6 +1026,14 @@ class FormFilling {
         //TODO:3: more accurate searching of submit buttons, etc. to avoid password resets if possible
         // maybe special cases for common HTML output patterns (e.g. javascript-only ASP.NET forms)
 
+        return submitElement;
+    }
+
+    // Submit a form
+    private submitForm (form: HTMLFormElement)
+    {
+        const submitElement = this.findSubmitButton(form);
+
         // Remember that it was KeeFox which initiated this form submission so we can
         // avoid searching for matching passwords upon submission
         //TODO:c: form submission
@@ -1041,24 +1051,24 @@ class FormFilling {
             form.submit();
         }
 
-            //TODO:2: maybe something like this might be useful? Dunno why a click()
-            // above wouldn't be sufficient but maybe some custom event raising might be handy...
-            /*
-            function simulateClick() {
-    var evt = document.createEvent("MouseEvents");
-    evt.initMouseEvent("click", true, true, window,
-        0, 0, 0, 0, 0, false, false, false, false, 0, null);
-    var cb = document.getElementById("checkbox");
-    var canceled = !cb.dispatchEvent(evt);
-    if(canceled) {
-        // A handler called preventDefault
-        alert("canceled");
-    } else {
-        // None of the handlers called preventDefault
-        alert("not canceled");
-    }
-    }
-    */
+        //TODO:3: maybe something like this might be useful? Dunno why a click()
+        // above wouldn't be sufficient but maybe some custom event raising might be handy...
+        /*
+        function simulateClick() {
+        var evt = document.createEvent("MouseEvents");
+        evt.initMouseEvent("click", true, true, window,
+            0, 0, 0, 0, 0, false, false, false, false, 0, null);
+        var cb = document.getElementById("checkbox");
+        var canceled = !cb.dispatchEvent(evt);
+        if(canceled) {
+            // A handler called preventDefault
+            alert("canceled");
+        } else {
+            // None of the handlers called preventDefault
+            alert("not canceled");
+        }
+        }
+        */
     }
 
     private calculateRelevanceScore (login, form,
