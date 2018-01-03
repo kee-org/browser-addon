d71fee9095697466d47cd47d957894078cea82fa
luckyrat <luckyrat@musites.com>
2018-01-03 23:13:07 +0000

    jhgjghjhg

--- WFT.sh
+++ WFT.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+set -ev
 KEE_VERSION_MANIFEST=`sed -nE 's/^.*"version": "([0-9]{1,}\.[0-9]{1,})\.[0-9x]{1,}"\,/\1/p' manifest.json`
 KEE_VERSION_TAG=`git describe --abbrev=0 --tags | sed -E 's/^([0-9]{1,}\.[0-9]{1,})\.([0-9]{1,})$/\1 \2/g'`
 echo "${KEE_VERSION_MANIFEST} ${KEE_VERSION_TAG}" | awk '{printf($1==$2?""$2"."$3+1:""$1".0")}' | xargs -I {} git tag -a {} -m "{}"
