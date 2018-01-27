8b251bf1e5574c47bb6d85003ad2b3428e992ba0
luckyrat <luckyrat@musites.com>
2018-01-27 23:40:14 +0000

    Fix: Submit elements were prioritised by reverse distance from form fields

--- page/formFilling.ts
+++ page/formFilling.ts
@@ -1057,8 +1057,8 @@ class FormFilling {
         });
 
         const submitElements = candidates.sort((a, b) => {
-            if (a.distance > b.distance) return 1;
-            if (a.distance < b.distance) return -1;
+            if (a.distance > b.distance) return -1;
+            if (a.distance < b.distance) return 1;
             return 0;
         });
 
