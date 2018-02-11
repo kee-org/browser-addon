e484fa44ddc53deb01a9421af56bdcc94c2a07b2
luckyrat <luckyrat@musites.com>
2018-02-11 11:59:12 +0000

    Sign and release new beta versions automatically

---
+++ prepareBetaRelease.sh
@@ -0,0 +1,11 @@
+echo "Cloning our beta update repo..."
+cd .. &&
+git clone https://github.com/kee-org/browser-addon-updates.git &&
+cd - &&
+echo "Signing debug build..." &&
+gulp sign &&
+faauv --update ../browser-addon-updates/beta/update.json --update-link $(cat .downloadLinkKeeXPI) dist/signed/$(cat .signedKeeXPI) &&
+cd ../browser-addon-updates &&
+git add . &&
+git commit -m "Automatic release of new beta version" &&
+cd -
