604e4b0c8225704d0eae52eae461013909318503
luckyrat <luckyrat@musites.com>
2018-03-14 16:05:11 +0000

    Debug: Try to get more info about where mystery travis error comes from

--- prepareBetaRelease.sh
+++ prepareBetaRelease.sh
@@ -2,14 +2,19 @@
 set -ev
 
 echo "Cloning our beta update repo..."
-cd .. &&
-git clone https://github.com/kee-org/browser-addon-updates.git &&
-cd - &&
-mkdir dist/signed &&
-echo "Signing debug build..." &&
-gulp sign &&
-faauv --update ../browser-addon-updates/beta/update.json --update-link $(cat .downloadLinkKeeXPI) dist/signed/$(cat .signedKeeXPI) &&
-cd ../browser-addon-updates &&
-git add . &&
-git commit -m "Automatic release of new beta version" &&
+cd ..
+git clone https://github.com/kee-org/browser-addon-updates.git
+cd -
+mkdir dist/signed
+echo "Signing debug build..."
+gulp sign
+echo "link:"
+cat .downloadLinkKeeXPI
+echo "XPI:"
+cat .signedKeeXPI
+faauv --update ../browser-addon-updates/beta/update.json --update-link $(cat .downloadLinkKeeXPI) dist/signed/$(cat .signedKeeXPI)
+cat ../browser-addon-updates/beta/update.json
+cd ../browser-addon-updates
+git add .
+git commit -m "Automatic release of new beta version"
 cd -
