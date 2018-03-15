82da1b3a4933461ebfdf5a4f6936f952ae08d972
luckyrat <luckyrat@musites.com>
2018-03-15 10:31:02 +0000

    Fix build: Add git credentials to updates repo too

--- prepareBetaRelease.sh
+++ prepareBetaRelease.sh
@@ -12,6 +12,8 @@ faauv --update ../browser-addon-updates/beta/update.json --update-link $(cat .do
 echo "New update manifest follows"
 cat ../browser-addon-updates/beta/update.json
 cd ../browser-addon-updates
+git config credential.helper "store --file=.git/credentials"
+echo "https://${GH_TOKEN}:@github.com" > .git/credentials
 git add .
 git commit -m "Automatic release of new beta version"
 cd -
