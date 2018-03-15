9fe36da7ad42c1d714ce65b745e9aed03769fe6e
luckyrat <luckyrat@musites.com>
2018-03-15 13:25:03 +0000

    Prevent uncaught context menu "duplicate ID" exception messages in console WebExtensions context menu API allows race conditions so this change wraps it in a simple instance variable that prevents concurrent execution leading to the insertion of multiple context entries with the same ID. We now log a debug message when this happens just in case we need to implement a more complex semaphore locking solution in future (i.e. if we need to delay subsequent API requests rather than just ignoring them)

--- background/commands.ts
+++ background/commands.ts
@@ -1,4 +1,6 @@
 class KFCommands {
+    private contextMenuUpdateLock = false;
+
     public init ()
     {
         browser.commands.onCommand.addListener(command => {
@@ -58,80 +60,25 @@ class KFCommands {
         });
     }
 
-    public async setupContextMenuItems () { // switch to promises?
-        await browser.contextMenus.removeAll();
-
-        if (kee.appState.connected && kee.appState.ActiveKeePassDatabaseIndex >= 0) {
-            try {
-                browser.contextMenus.create({
-                    id: Command.DetectForms,
-                    title: $STR("Menu_Button_fillCurrentDocument_label"),
-                    contexts: [ browser.contextMenus.ContextType.EDITABLE,
-                            browser.contextMenus.ContextType.FRAME,
-                            browser.contextMenus.ContextType.IMAGE,
-                            browser.contextMenus.ContextType.LINK,
-                            browser.contextMenus.ContextType.PAGE,
-                            browser.contextMenus.ContextType.PASSWORD,
-                            browser.contextMenus.ContextType.SELECTION ]
-                });
-            } catch (e) {
-                // try again with Chrome-supported contexts
-                browser.contextMenus.create({
-                    id: Command.DetectForms,
-                    title: $STR("Menu_Button_fillCurrentDocument_label"),
-                    contexts: [ browser.contextMenus.ContextType.EDITABLE,
-                        browser.contextMenus.ContextType.FRAME,
-                        browser.contextMenus.ContextType.IMAGE,
-                        browser.contextMenus.ContextType.LINK,
-                        browser.contextMenus.ContextType.PAGE,
-                        browser.contextMenus.ContextType.SELECTION ]
-                });
-            }
+    public async setupContextMenuItems () {
+        if (commandManager.contextMenuUpdateLock) {
+            KeeLog.debug("If you are missing entries from your context menu, we will need" +
+                " to spend more effort on the setupContextMenuItems implementation (wait" +
+                " by using semaphores rather than assuming new search results always follow" +
+                " setup requests with no results)");
+            return;
         }
 
-        if (kee.appState.connected) {
-            try {
-                browser.contextMenus.create({
-                    id: Command.GeneratePassword,
-                    title: $STR("Menu_Button_copyNewPasswordToClipboard_label"),
-                    contexts: [ browser.contextMenus.ContextType.EDITABLE,
-                        browser.contextMenus.ContextType.FRAME,
-                        browser.contextMenus.ContextType.IMAGE,
-                        browser.contextMenus.ContextType.LINK,
-                        browser.contextMenus.ContextType.PAGE,
-                        browser.contextMenus.ContextType.PASSWORD,
-                        browser.contextMenus.ContextType.SELECTION ]
-            });
-            } catch (e) {
-                // try again with Chrome-supported contexts
-                browser.contextMenus.create({
-                    id: Command.GeneratePassword,
-                    title: $STR("Menu_Button_copyNewPasswordToClipboard_label"),
-                    contexts: [ browser.contextMenus.ContextType.EDITABLE,
-                        browser.contextMenus.ContextType.FRAME,
-                        browser.contextMenus.ContextType.IMAGE,
-                        browser.contextMenus.ContextType.LINK,
-                        browser.contextMenus.ContextType.PAGE,
-                        browser.contextMenus.ContextType.SELECTION ]
-                });
-            }
-        }
+        commandManager.contextMenuUpdateLock = true;
+        try {
+            await browser.contextMenus.removeAll();
 
-        // if (kee.appState.ActiveKeePassDatabaseIndex < 0) {
-
-        // }
-
-        if (kee.foregroundTabId >= 0
-        && kee.tabStates.has(kee.foregroundTabId)
-        && kee.tabStates.get(kee.foregroundTabId).frames) {
-            kee.tabStates.get(kee.foregroundTabId).frames.forEach(frame => {
-                for (let j=0; j<frame.logins.length; j++) {
-                    const login = frame.logins[j];
-                    try {
-                        browser.contextMenus.create({
-                            id: "matchedLogin-" + j,
-                            title: login.title,
-                            contexts: [ browser.contextMenus.ContextType.EDITABLE,
+            if (kee.appState.connected && kee.appState.ActiveKeePassDatabaseIndex >= 0) {
+                try {
+                    browser.contextMenus.create({
+                        id: Command.DetectForms,
+                        title: $STR("Menu_Button_fillCurrentDocument_label"),
+                        contexts: [ browser.contextMenus.ContextType.EDITABLE,
                                 browser.contextMenus.ContextType.FRAME,
                                 browser.contextMenus.ContextType.IMAGE,
                                 browser.contextMenus.ContextType.LINK,
@@ -139,21 +86,85 @@ class KFCommands {
                                 browser.contextMenus.ContextType.PASSWORD,
                                 browser.contextMenus.ContextType.SELECTION ]
                     });
-                    } catch (e) {
-                        // try again with Chrome-supported contexts
-                        browser.contextMenus.create({
-                            id: "matchedLogin-" + j,
-                            title: login.title,
-                            contexts: [ browser.contextMenus.ContextType.EDITABLE,
-                                browser.contextMenus.ContextType.FRAME,
-                                browser.contextMenus.ContextType.IMAGE,
-                                browser.contextMenus.ContextType.LINK,
-                                browser.contextMenus.ContextType.PAGE,
-                                browser.contextMenus.ContextType.SELECTION ]
-                        });
-                    }
+                } catch (e) {
+                    // try again with Chrome-supported contexts
+                    browser.contextMenus.create({
+                        id: Command.DetectForms,
+                        title: $STR("Menu_Button_fillCurrentDocument_label"),
+                        contexts: [ browser.contextMenus.ContextType.EDITABLE,
+                            browser.contextMenus.ContextType.FRAME,
+                            browser.contextMenus.ContextType.IMAGE,
+                            browser.contextMenus.ContextType.LINK,
+                            browser.contextMenus.ContextType.PAGE,
+                            browser.contextMenus.ContextType.SELECTION ]
+                    });
                 }
-            });
+            }
+
+            if (kee.appState.connected) {
+                try {
+                    browser.contextMenus.create({
+                        id: Command.GeneratePassword,
+                        title: $STR("Menu_Button_copyNewPasswordToClipboard_label"),
+                        contexts: [ browser.contextMenus.ContextType.EDITABLE,
+                            browser.contextMenus.ContextType.FRAME,
+                            browser.contextMenus.ContextType.IMAGE,
+                            browser.contextMenus.ContextType.LINK,
+                            browser.contextMenus.ContextType.PAGE,
+                            browser.contextMenus.ContextType.PASSWORD,
+                            browser.contextMenus.ContextType.SELECTION ]
+                });
+                } catch (e) {
+                    // try again with Chrome-supported contexts
+                    browser.contextMenus.create({
+                        id: Command.GeneratePassword,
+                        title: $STR("Menu_Button_copyNewPasswordToClipboard_label"),
+                        contexts: [ browser.contextMenus.ContextType.EDITABLE,
+                            browser.contextMenus.ContextType.FRAME,
+                            browser.contextMenus.ContextType.IMAGE,
+                            browser.contextMenus.ContextType.LINK,
+                            browser.contextMenus.ContextType.PAGE,
+                            browser.contextMenus.ContextType.SELECTION ]
+                    });
+                }
+            }
+
+            if (kee.foregroundTabId >= 0
+            && kee.tabStates.has(kee.foregroundTabId)
+            && kee.tabStates.get(kee.foregroundTabId).frames) {
+                kee.tabStates.get(kee.foregroundTabId).frames.forEach(frame => {
+                    for (let j=0; j<frame.logins.length; j++) {
+                        const login = frame.logins[j];
+                        try {
+                            browser.contextMenus.create({
+                                id: "matchedLogin-" + j,
+                                title: login.title,
+                                contexts: [ browser.contextMenus.ContextType.EDITABLE,
+                                    browser.contextMenus.ContextType.FRAME,
+                                    browser.contextMenus.ContextType.IMAGE,
+                                    browser.contextMenus.ContextType.LINK,
+                                    browser.contextMenus.ContextType.PAGE,
+                                    browser.contextMenus.ContextType.PASSWORD,
+                                    browser.contextMenus.ContextType.SELECTION ]
+                        });
+                        } catch (e) {
+                            // try again with Chrome-supported contexts
+                            browser.contextMenus.create({
+                                id: "matchedLogin-" + j,
+                                title: login.title,
+                                contexts: [ browser.contextMenus.ContextType.EDITABLE,
+                                    browser.contextMenus.ContextType.FRAME,
+                                    browser.contextMenus.ContextType.IMAGE,
+                                    browser.contextMenus.ContextType.LINK,
+                                    browser.contextMenus.ContextType.PAGE,
+                                    browser.contextMenus.ContextType.SELECTION ]
+                            });
+                        }
+                    }
+                });
+            }
+        } finally {
+            commandManager.contextMenuUpdateLock = false;
         }
     }
 }
