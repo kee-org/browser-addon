012458e42bd42ac9dcc4e9e5d967b9dfeb2e7e43
luckyrat <luckyrat@musites.com>
2019-06-22 12:00:57 +0100

    Track expected mutation hook behaviour

--- store/syncContent.ts
+++ store/syncContent.ts
@@ -1,5 +1,6 @@
 import { KeeState } from "./KeeState";
 import { Store, MutationPayload } from "vuex";
+import { KeeLog } from "../common/Logger";
 
 export class SyncContent {
     initialized: any;
@@ -39,10 +40,15 @@ export class SyncContent {
         if (!this.receivedMutations.length) {
             return this.sendMutation(mutation);
         }
+        if (this.receivedMutations.length > 1) {
+            KeeLog.error("Misunderstood mutation hook ordering. Bugs ahoy!");
+        }
 
         // Check if it's received mutation, if it's just ignore it, if not send to background
         for (let i = this.receivedMutations.length - 1; i >= 0; i--) {
             if (this.receivedMutations[i].type == mutation.type && this.receivedMutations[i].payload == mutation.payload) {
+                // If multiple mutations can be in received queue, think we should break here... otherwise duplicate received
+                // mutations could be sent cos more than one will have been removed from the list of known local mutations
                 this.receivedMutations.splice(i, 1);
             } else if (i == 0) {
                 this.sendMutation(mutation);
@@ -57,8 +63,6 @@ export class SyncContent {
 
         for (let i = 0; i < this.pendingMutations.length; i++) {
             this.store.commit(this.pendingMutations[i].type, this.pendingMutations[i].payload);
-
-            // Clean the pending mutation when are applied
             this.pendingMutations.splice(i, 1);
         }
     }
