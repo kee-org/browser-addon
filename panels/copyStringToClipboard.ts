9b6814d27465796a20dd0c8b8d7844d606ec53df
luckyrat <luckyrat@musites.com>
2020-10-06 13:05:30 +0100

    Workaround clipboard copying bug
    Fixes #273

--- panels/copyStringToClipboard.ts
+++ panels/copyStringToClipboard.ts
@@ -4,6 +4,19 @@ export async function copyStringToClipboard(value: string) {
     try {
         await navigator.clipboard.writeText(value);
     } catch (e) {
-        KeeLog.error("Failed to write to clipboard");
+        try {
+            // Fallback to old textarea hack if required.
+            // See https://github.com/kee-org/browser-addon/issues/273
+            const copyFrom = document.createElement("textarea");
+            copyFrom.textContent = value;
+            const body = document.getElementsByTagName("body")[0];
+            body.appendChild(copyFrom);
+            copyFrom.select();
+            document.execCommand("copy");
+            body.removeChild(copyFrom);
+            KeeLog.info("Failed to write to clipboard using modern API so used the fallback hack");
+        } catch (e2) {
+            KeeLog.error("Failed to write to clipboard using modern API and fallback hack");
+        }
     }
 }
