20ba3be6fab92af81b55b2930ad74e5f3e1aba38
luckyrat <luckyrat@musites.com>
2023-02-13 13:27:01 +0000

    Replace group selector tree interface
    Re-enables filtering of group names when selecting where to save a new
    entry.
    
    Also improves the appearance and usability slightly.
    
    Contracted group status is no longer remembered when the filter changes.
    
    Keyboard shortcuts are marginally improved since earlier versions (it's
    possible to select an item using the enter key) but still far from ideal
    (see https://www.w3.org/WAI/ARIA/apg/patterns/treeview/ for all the
    details we haven't had time to implement yet)

--- src/popup/components/SaveWhere.vue
+++ src/popup/components/SaveWhere.vue
@@ -10,39 +10,17 @@
                 <v-row style="margin-top: 0px;">
                     <v-col>
                         <v-card class="mx-auto" max-width="500">
-                            <!-- TODO: restore search feature when we have time to setup a new/improved treeview component that supports it -->
-                            <!-- <v-sheet class="pa-4 pb-1">
-                                <v-text-field v-model="search" :label="$i18n('Search_label')" color="secondary"
-                                    variant="outlined" hide-details="auto" clearable density="compact"
+                            <v-sheet class="pa-4 pb-1">
+                                <v-text-field
+                                    v-model="debouncedSearchValue" :label="$i18n('Search_label')"
+                                    color="secondary" variant="outlined" hide-details="auto" clearable density="compact"
                                     clear-icon="mdi-close-circle-outline" />
-                            </v-sheet> -->
+                            </v-sheet>
                             <v-card-text>
-                                <tree-view ref="treeViewRef" id="my-tree" :initial-model="items" selectionMode="single"
-                                    :modelDefaults="{
-                                        selectable: true,
-                                        idProperty: 'uuid',
-                                        labelProperty: 'title',
-                                        childrenProperty: 'groups',
-                                        expandable: true,
-                                        state: {
-                                            expanded: true
-                                        }
-                                    }" style="height: 225px; overflow-y: auto; cursor: pointer"
-                                    @treeNodeSelectedChange="setGroup">
-                                    <template #text="{ model }">
-                                        <v-container>
-                                            <v-btn v-if="model.groups.length > 0" icon size="1.5rem" color="secondary"
-                                                style="margin-left:0.5rem; margin-right: 0.5rem;"
-                                                @click="$event.currentTarget.parentElement.previousElementSibling.click()">
-                                                <mdi-chevron-up v-if="model.treeNodeSpec.state.expanded" scale="150" />
-                                                <mdi-chevron-down v-if="!(model.treeNodeSpec.state.expanded)"
-                                                    scale="150" />
-                                            </v-btn>
-                                            {{ model.title }}
-                                        </v-container>
-                                    </template>
-
-                                </tree-view>
+                                <Tree
+                                    :items="items" :filter-text="search ?? ''"
+                                    style="max-height: 220px;min-height: 220px;overflow-y: auto;"
+                                    @set-group="(item: GroupListItem) => handleSelect(item)"></tree>
                             </v-card-text>
                         </v-card>
                     </v-col>
@@ -78,8 +56,10 @@ import { GroupSummary, TemporaryIDString } from "../../common/model/GroupSummary
 import { configManager } from "../../common/ConfigManager";
 import useStore from "../../store";
 import { mapState } from "pinia";
-import { TreeView } from "@grapoza/vue-tree"
-//import { KeeLog } from "~/common/Logger";
+import { KeeLog } from "~/common/Logger";
+import GroupSelectorTree from "./GroupSelectorTree.vue";
+import { GroupListItem } from "./TreeItem.vue";
+
 
 const recursiveFilterMap = (group: Group, filter: (grp: Group) => boolean, map: (grp: Group) => Group): Group => {
     const chosenChildGroups: Group[] = [];
@@ -97,38 +77,24 @@ const recursiveFilterMap = (group: Group, filter: (grp: Group) => boolean, map: 
         return map(g);
     }
     return null;
+};
+
+function findDatabaseAndGroupByGroupId(databases: Database[], uuid: string) {
+
+    for (const database of databases) {
+        const group = Group.matchingId(database.root, uuid);
+        if (group) {
+            return { database, group };
+        }
+    }
+    return null;
 }
 
-function findDatabaseByGroup(databases: Database[], group: Group) {
-    return databases.find(db => Group.containsId(db.root, group.uuid));
-}
-
-// const findById = (uuid: String) => (xs) =>
-//     xs.reduce(
-//         (res, x: Group) => res ? res : x.uuid === uuid ? x : findById(uuid)(x.groups || []),
-//         undefined
-//     )
-
-// function populateShadowTreeLevel(shadowTreeLevel: any[], treeArrayLevel: any[]) {
-//     for (const shadowItem of shadowTreeLevel) {
-//         const correspondingTreeItem = treeArrayLevel.find(i => i.uuid === shadowItem.uuid);
-//         if (correspondingTreeItem) {
-//             shadowItem.nodeTreeSpec = correspondingTreeItem.nodeTreeSpec;
-//             populateShadowTreeLevel(shadowItem.groups, correspondingTreeItem.groups);
-//         }
-//     }
-// }
-
-
-//const treeArray = [];
-
-// can't be reactive
-// needs to just cache a copy of the real tree temporarilly
-//let shadowTree = [];
+let selectedItem: any = {};
 
 export default {
     components: {
-        TreeView
+        Tree: GroupSelectorTree
     },
     props: ["preferredGroupUuid", "displayReason"],
     setup() {
@@ -138,6 +104,7 @@ export default {
     data: function () {
         return {
             search: "",
+            debounceTimeout: null,
             updatingConfig: false,
             skipInFuture: configManager.current.rememberMRUGroup,
             lastSelectedGroupUuid: this.$props.preferredGroupUuid, // caller has already checked that this uuid is present in list of DBs and groups
@@ -152,9 +119,20 @@ export default {
         saveEnabled: function () {
             return !this.updatingConfig && this.lastSelectedGroupUuid?.length > 0;
         },
+        debouncedSearchValue: {
+            get() {
+                return this.search;
+            },
+            set(value) {
+                clearTimeout(this.debounceTimeout);
+                this.debounceTimeout = setTimeout(() => {
+                    this.search = value;
+                }, 50);
+            }
+        },
         items: function () {
             // array of all databases root group
-            const groups = this.KeePassDatabases.map(db => recursiveFilterMap(db.root, g => true, (g) => {
+            const groups = this.KeePassDatabases.map(db => recursiveFilterMap(db.root, g => true, g => {
                 const grp = new Group({
                     entrySummaries: [],
                     groups: g.groups,
@@ -164,55 +142,29 @@ export default {
                     uuid: g.uuid
                 }
                 );
-                if (g.uuid === this.initialSelectedGroupId) {
-                    (grp as any).treeNodeSpec = {
-                        state: {
-                            selected: true
-                        }
-                    }
-                }
                 return grp;
             }));
-            //const groups = this.KeePassDatabases.map(db => db.root);
-            const json = JSON.stringify(groups);
-            //KeeLog.debug("items: ", groups);
-            return JSON.parse(json) as Group[];
-        },
-        // filteredItems: function () {
 
-        //     //TODO: try selected: false for all others if multiselection bug still happens
+            const mapItem = (item: Group) => {
+                const selected = item.uuid === this.initialSelectedGroupId;
+                const gli = new GroupListItem(
+                    item.uuid,
+                    item.title,
+                    item.groups.map(mapItem),
+                    selected,
+                    []
 
-        //     // Need to keep this to trick vue into recomputing when search term is modified
-        //     //TODO: verify above and check for a less noisy solution
-        //     console.log("search hack: " + this.search);
-
-        //     if (treeArray.length > 0) {
-        //         // Treeview component may have changed treenodespec since we last ran this computed value
-        //         // We have to clone to avoid infinite references continually trigger re-evaluation of this property
-
-        //         // for every item in the shadow tree, we need to update it with whatever is currently stored in the corresponding item in the treeArray
-        //         //Shadow tree will always contain all items but real tree might not, and might have updated some of the data for the items that are in the shdow tree
-
-        //         populateShadowTreeLevel(shadowTree, treeArray);
-
-        //     }
-
-        //     const result = this.items.map(item => recursiveFilterMap(
-        //         item,
-        //         (grp) => {
-        //             const s = (this.search ?? "").toLowerCase();
-        //             return grp.title.toLowerCase().includes(s);
-        //         },
-        //         (clonedGroup) => {
-        //             return findById(clonedGroup.uuid)(shadowTree);
-        //         })
-        //     ).filter(i => i); // filtering removes any nulls if entire top level objects have been filtered away
-        //     const jsoned = JSON.stringify(result);
-        //     KeeLog.debug("jsoned", jsoned);
-        //     treeArray.length = 0;
-        //     treeArray.push(...result);
-        //     return treeArray;
-        // }
+                );
+                // We need to cache the reference to a reactive version of the item if it has been preselected
+                // so we can deselect it later. The other items will be Proxified later.
+                const result = selected ? reactive(gli) : gli;
+                if (selected) {
+                    selectedItem = result;
+                }
+                return result;
+            };
+            return reactive(groups.map(mapItem));
+        }
     },
     watch: {
         skipInFuture: async function (newValue: boolean, oldValue: boolean) {
@@ -227,50 +179,19 @@ export default {
     mounted() {
         // Not certain that mounted is the right place for this but maybe it's fine
         if (this.initialSelectedGroupId?.length > 0) {
-            const selected = (this.$refs.treeViewRef as any).getSelected();
-            const selectedGroup = selected?.[0];
-            if (selectedGroup) {
-                this.setGroup(selectedGroup);
-            }
+            this.setGroupById(this.initialSelectedGroupId);
         }
-
-        // const selectionObj = {
-        //     treeNodeSpec: {
-        //         state: {
-        //             selected: true
-        //         }
-        //     }
-        // };
-
-        // // Could maybe just use this.items but a seperate shadow tree is probably less risky in case other changes cause items to react
-        // shadowTree = this.items.map(item => recursiveFilterMap(
-        //     item,
-        //     g => true,
-        //     //TODO: unnecessary to use a cloned group here I think
-        //     (clonedGroup) => clonedGroup.uuid === this.lastSelectedGroupUuid ? Object.assign(clonedGroup, selectionObj) : clonedGroup)
-        // );
     },
     methods: {
-        blah: function (el: any) {
-            console.log(el);
-        },
+
         saveEntry: function () {
             Port.postMessage({ action: Action.CreateEntry } as AddonMessage);
             window.close();
         },
-        setGroup: function (group: Group) {
-            if (!group?.uuid) return;
+        setGroupById: function (uuid: string) {
+            if (!uuid) return;
 
-            // We are sent the deselection event too (but often (always?) after the selection event so it is impossible to re-disable the Save button safely)
-
-            if (!(group as any).treeNodeSpec?.state?.selected) {
-                //this.saveEnabled = false;
-                return;
-            }
-
-            const selectedGroup = JSON.parse(JSON.stringify(group));
-            delete selectedGroup.treeNodeSpec;
-            const database = findDatabaseByGroup(this.KeePassDatabases, selectedGroup);
+            const { database, group: selectedGroup } = findDatabaseAndGroupByGroupId(this.KeePassDatabases, uuid);
 
             // Maybe object assign isn't actually removing the proxy objects?
             const updatedSaveState = Object.assign({}, this.saveState) as SaveState;
@@ -285,103 +206,16 @@ export default {
                 })
             });
             this.updateSaveState(updatedSaveState);
-            this.lastSelectedGroupUuid = selectedGroup.uuid;
+            this.lastSelectedGroupUuid = uuid;
+        },
+        handleSelect(item: GroupListItem) {
+            KeeLog.debug("Setting selected group to " + item.id);
+            selectedItem.selected = false;
+            item.selected = true;
+            selectedItem = item;
+            this.setGroupById(item.id);
         }
     }
 };
 
 </script>
-
-<style>
-.grtv-wrapper.grtv-default-skin {
-    --baseHeight: 1rem;
-    --itemSpacing: 1rem
-}
-
-.grtv-wrapper.grtv-default-skin .grtvn {
-    padding-left: 0
-}
-
-.grtv-wrapper.grtv-default-skin .grtvn:first-child {
-    margin-top: 0
-}
-
-.grtv-wrapper.grtv-default-skin .grtvn[role=treeitem]:focus {
-    outline: 0
-}
-
-.grtv-wrapper.grtv-default-skin .grtvn[role=treeitem]:focus>.grtvn-self {
-    outline: black dotted 1px
-}
-
-.grtv-wrapper.grtv-default-skin .grtvn-self {
-    display: flex;
-    align-items: flex-start;
-    line-height: var(--baseHeight)
-}
-
-.grtv-wrapper.grtv-default-skin .grtvn-self-expander {
-    padding: 0;
-    background: none;
-    border: none;
-    height: var(--baseHeight)
-}
-
-.grtv-wrapper.grtv-default-skin .grtvn-self-selected {
-    background: rgb(var(--v-theme-primary));
-    color: rgb(var(--v-theme-on-primary));
-    font-weight: bold;
-}
-
-.grtv-wrapper.grtv-default-skin .grtvn-self:hover {
-    background: rgb(var(--v-theme-surface-variant));
-    color: rgb(var(--v-theme-on-surface-variant));
-    font-weight: bold;
-}
-
-.grtv-wrapper.grtv-default-skin .grtvn-self-expander,
-.grtv-wrapper.grtv-default-skin .grtvn-self-checkbox,
-.grtv-wrapper.grtv-default-skin .grtvn-self-radio,
-.grtv-wrapper.grtv-default-skin .grtvn-self-spacer,
-.grtv-wrapper.grtv-default-skin .grtvn-self-action {
-    min-width: 0.5rem
-}
-
-.grtv-wrapper.grtv-default-skin>.grtv>.grtvn>.grtvn-self>.grtvn-self-expander {
-    min-width: 0
-}
-
-.grtv-wrapper.grtv-default-skin .grtvn-self>.v-container {
-    padding: 0;
-    min-height: 40px;
-    display: flex;
-    align-content: center;
-    justify-content: start;
-    align-items: center;
-}
-
-.grtv-wrapper.grtv-default-skin .grtvn-self-expander,
-.grtv-wrapper.grtv-default-skin .grtvn-self-spacer {
-    margin: 0
-}
-
-.grtv-wrapper.grtv-default-skin .grtvn-self-text,
-.grtv-wrapper.grtv-default-skin .grtvn-self-label {
-    margin-left: var(--itemSpacing)
-}
-
-.grtv-wrapper.grtv-default-skin .grtvn-children-wrapper {
-    margin: 0 0 0 calc(1rem + var(--itemSpacing))
-}
-
-.grtv-wrapper.grtv-default-skin .grtvn-children {
-    padding: 0;
-    list-style: none
-}
-
-.grtv-wrapper.grtv-default-skin>.grtv {
-    margin: 0;
-    padding: 0;
-    list-style: none
-}
-</style>
